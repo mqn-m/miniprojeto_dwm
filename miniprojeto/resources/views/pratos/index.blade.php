738aea0232284caae9ecfb3a23d429e6c17a8302
Ozzy <austin.foote@ua.pt>
2020-10-24 17:09:56 +0100

    css de pratos.index

--- miniprojeto/resources/views/pratos/index.blade.php
+++ miniprojeto/resources/views/pratos/index.blade.php
@@ -1,22 +1,23 @@
 @extends('layout')
 
 @section('content')
-<section id="three" class="wrapper">
-    <h1 class="titulo">Pratos </h1>
+<section id="Pratos" class="wrapper">
     <div class="inner" style="text-align: right;">
+        <h1 class="titulo">Pratos </h1>
         <a href="pratos.novo" class="button special small">Adicionar</a>
     </div>
-    <div class="inner flex flex-3">
+    <div class="inner">
         @foreach($pratos as $prato)
-        <div class="flex-item box">
-            <div class="content">
-                <h3>{{ $prato->nome }}</h3>
-                <p>{{ $prato->nota}}</p>
-                <h4>{{ $prato->cal}} cals</h4>
-
+            <div class="cartao">
+                <div class="content">
+                    <h3>{{ $prato->nome }}</h3>
+                    <p>{{ $prato->nota }}</p>
+                    <h4>{{ $prato->cal }} cals</h4>
+                    <a href="" class="button small editar">Editar</a>
+                </div>
             </div>
-        </div>
         @endforeach
     </div>
+
 </section>
 @endsection
