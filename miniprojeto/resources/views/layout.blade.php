5c78536dfeecf6a7ccc0aaa7067e484698540be9
Ozzy <austin.foote@ua.pt>
2020-10-24 18:02:29 +0100

    layout fix

--- miniprojeto/resources/views/layout.blade.php
+++ miniprojeto/resources/views/layout.blade.php
@@ -10,10 +10,6 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="/assets/css/main.css" />
-<<<<<<< Updated upstream
-=======
-		<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css"> -->
->>>>>>> Stashed changes
 	</head>
 	<body>
 
