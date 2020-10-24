1c249cf88c0a249f1f3bebd6847a9621767428d7
Maria <maria.nobre@ua.pt>
2020-10-24 09:08:00 +0100

    miniprojeto que austin me enviou ainda nao fiz lhe nada

---
+++ miniprojeto/app/Http/Controllers/PratoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Prato;
+use Illuminate\Http\Request;
+
+class PratoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pratos = Prato::latest()->get();
+        return view('pratos.index', ['pratos' => $pratos]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Prato  $Prato
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Prato $Prato)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Prato  $Prato
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Prato $Prato)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Prato  $Prato
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Prato $Prato)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Prato  $Prato
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Prato $Prato)
+    {
+        //
+    }
+}
