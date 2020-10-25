1f43b86f3be73d702df91392ab8d8160f7db8b98
Ozzy <austin.foote@ua.pt>
2020-10-25 10:19:14 +0000

    Extreme CSS OCD  (╯°□°）╯︵ ┻━┻

--- miniprojeto/resources/views/pratos/index.blade.php
+++ miniprojeto/resources/views/pratos/index.blade.php
@@ -13,7 +13,8 @@
                     <h3>{{ $prato->nome }}</h3>
                     <p>{{ $prato->nota }}</p>
                     <h4>{{ $prato->cal }} cals</h4>
-                <a href="pratos/editar/{{ $prato->id }}" class="button small editar">Editar</a>
+                <a href="pratos/editar/{{ $prato->id }}" class=" icon fa-edit botao editar"></a>
+                <a href="" class=" icon fa-trash botao apagar"></a>
                 </div>
             </div>
         @endforeach
