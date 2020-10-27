1700f785dd346f96fba88187adbaf3b89120c86a
mqn-m <57487639+mqn-m@users.noreply.github.com>
2020-10-27 17:48:35 +0000

    tabela de associação many to many, refeicoes_pratos

--- miniprojeto/database/migrations/2020_10_24_150423_create_refeicoes_table.php
+++ miniprojeto/database/migrations/2020_10_24_150423_create_refeicoes_table.php
@@ -15,12 +15,23 @@ class CreateRefeicoesTable extends Migration
     {
         Schema::create('refeicaos', function (Blueprint $table) {
             $table->id();
-            $table->enum('altura_dia', ['Pequeno-Almoço', 'Almoço','Jantar','Lanche manhã','Lanche tarde','Ceia']);
+            $table->enum('altura_dia', ['Pequeno-Almoço', 'Almoço', 'Jantar', 'Lanche manhã', 'Lanche tarde', 'Ceia']);
             $table->date('data_refeicao');
             $table->integer('total_cal');
             $table->text('notas')->nullable();
             $table->integer('user')->nullable();
             $table->timestamps();
+        });
+
+        Schema::create('refeicaos_pratos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('refeicaos_id');
+            $table->unsignedBigInteger('pratos_id');
+            $table->timestamps();
+
+            $table->unique(['refeicaos_id', 'pratos_id']);
+            $table->foreign('pratos_id')->references('id')->on('pratos')->onDelete('cascade');
+            $table->foreign('refeicaos_id')->references('id')->on('refeicaos')->onDelete('cascade');
         });
     }
 
