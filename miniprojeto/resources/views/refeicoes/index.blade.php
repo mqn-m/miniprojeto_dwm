744c6732704af96e4aab9a7a803a417951d241d4
mqn-m <57487639+mqn-m@users.noreply.github.com>
2020-10-28 17:59:09 +0000

    Update index.blade.php

--- miniprojeto/resources/views/refeicoes/index.blade.php
+++ miniprojeto/resources/views/refeicoes/index.blade.php
@@ -6,16 +6,19 @@
     <div class="inner" style="text-align: right;">
         <a href="refeicoes/novo" class="button special small">Adicionar</a>
     </div>
+    
     <div class="inner flex flex-3">
         @foreach($refeicaos as $refeicao)
-        <div class="flex-item box">
-            <div class="content">
-                <h3>{{ $refeicao->altura_dia }}</h3>
-                <h3>{{ $refeicao->data_refeicao }}</h3>
-                <p>{{ $refeicao->notas}}</p>
-                <h4>{{ $refeicao->total_cal}} cals</h4>
+        <a href="/refeicoes/{{ $refeicao->id }}">
+            <div class="flex-item box">
+                <div class="content">
+                    <h3>{{ $refeicao->altura_dia }}</h3>
+                    <h3>{{ $refeicao->data_refeicao }}</h3>
+                    <p>{{ $refeicao->notas}}</p>
+                    <h4>{{ $refeicao->total_cal}} cals</h4>
+                </div>
             </div>
-        </div>
+        </a>
         @endforeach
     </div>
 </section>
