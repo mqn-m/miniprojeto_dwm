0fd5e6338d6734460dc73d6ee127ec44bdf61447
Ozzy <austin.foote@ua.pt>
2020-11-05 16:03:21 +0000

    Pagina de login feito

--- miniprojeto/resources/views/auth/perfil.blade.php
+++ miniprojeto/resources/views/auth/perfil.blade.php
@@ -1,71 +1,57 @@
 @extends('layout')
 
 @section('content')
-<div class="wrapper">
-    <div class="">
+    <div class="wrapper">
         <div class="inner">
-            
-                <h1 class="titulo">{{ __('Alterar Perfil') }}</h1>
+            <h1 class="titulo">{{ __('Alterar Perfil') }}</h1>
+            <div class="content">
 
-                <div class="content">
-                    <form method="POST" action="/perfil/{{ Auth::user()->id }}">
-                        @csrf
-                        @method('PUT')
+                <form method="POST" action="/perfil/{{ Auth::user()->id }}">
+                    @csrf
+                    @method('PUT')
 
-                        <div class="row uniform">
+                    <div class="row uniform">
                         <div class="12u$">
                             <label for="name" class="">{{ __('Nome') }}</label>
-
-                            
-                                <input id="name" type="text" name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
-                                
-                            </div>
+                            <input id="name" type="text" name="name" value="{{ Auth::user()->name }}" required
+                                autocomplete="name" autofocus>
                         </div>
+                    </div>
 
-                        <div class="row uniform">
+                    <div class="row uniform">
                         <div class="12u$">
                             <label for="email" class="">{{ __('E-Mail') }}</label>
-
-                            
-                                <input id="email" type="email" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
-                                
-
-                            </div>
+                            <input id="email" type="email" name="email" value="{{ Auth::user()->email }}" required
+                                autocomplete="email">
                         </div>
+                    </div>
 
-                        <div class="row uniform">
+                    <div class="row uniform">
                         <div class="12u$">
                             <label for="idade" class="">{{ __('Idade') }}</label>
-
-                            
-                                <input id="idade" type="number" name="idade" value="{{ Auth::user()->idade }}" required autocomplete="idade">
-                                
-
-                            </div>
+                            <input id="idade" type="number" name="idade" value="{{ Auth::user()->idade }}" required
+                                autocomplete="idade">
                         </div>
+                    </div>
 
-                        <div class="row uniform">
+                    <div class="row uniform">
                         <div class="12u$">
                             <label for="peso" class="">{{ __('Peso') }}</label>
-
-                            
-                                <input id="peso" type="number" step="0.010" name="peso" value="{{ Auth::user()->peso }}" required autocomplete="peso">
-                                
-
-                            </div>
+                            <input id="peso" type="number" step="0.010" name="peso" value="{{ Auth::user()->peso }}"
+                                required autocomplete="peso">
                         </div>
-                        <br>
+                    </div>
+                    <br>
 
-                        
-                            <div class="actions">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Guardar Alterações') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    <div class="actions">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Guardar Alterações') }}
+                        </button>
+                    </div>
+
+                </form>
+            </div>
+
         </div>
     </div>
-</div>
 @endsection
--- miniprojeto/resources/views/layout.blade.php
+++ miniprojeto/resources/views/layout.blade.php
@@ -1,19 +1,10 @@
 <!DOCTYPE HTML>
 <!--
-	Intensify by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+ Intensify by TEMPLATED
+ templated.co @templatedco
+ Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 -->
 <html>
-	<head>
-		<title>Food Tracker</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="/assets/css/main.css" />
-		<link rel="icon" href="/images/Flogo.png">
-		@yield('style')
-	</head>
-	<body>
 
 <head>
     <title>Food Tracker</title>
@@ -21,67 +12,86 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="/assets/css/main.css" />
     <link rel="icon" href="/images/Flogo.png">
+    @yield('style')
 </head>
 
 <body>
 
+    <head>
+        <title>Food Tracker</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="/assets/css/main.css" />
+        <link rel="icon" href="/images/Flogo.png">
+    </head>
 
-    <!-- Header -->
-    <header id="header">
-        <nav class="left">
-            <a href="#menu"><span>Menu</span></a>
-        </nav>
-        <a href="/" class="logo">Food Tracker</a>
-        <nav class="right">
-          Bem vindo(a),  <a href="/perfil" class="nome_perfil fit alt" >{{ Auth::user()->name }}</a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-				@csrf
-			<button type="submit" class="button small">LOG OUT</button>
-            </form>
+    <body>
 
-        </nav>
-    </header>
 
-    <!-- Menu -->
-    <nav id="menu">
-        <ul class="links">
-            <li><a href="/">Home</a></li>
-            <li><a href="/pratos">Pratos</a></li>
-            <li><a href="/refeicoes">Refeicões</a></li>
-            <li><a href="/perfil">Perfil</a></li>
-        </ul>
-        <ul class="actions vertical">
-            <li> <form action="{{ route('logout') }}" method="POST" class="d-none">
-				@csrf
-			<button type="submit" class="button fit">LOG OUT</button>
-            </form> </li> <!-- log out -->
-        </ul>
-    </nav>
+        <!-- Header -->
 
-    @yield('content')
+        <header id="header">
+            @if (Auth::check())
+                <nav class="left">
+                    <a href="#menu"><span>Menu</span></a>
+                </nav>
+            @endif
+            <a href="/" class="logo">Food Tracker</a>
+            @if (Auth::check())
+                <nav class="right">
+                        Bem vindo(a), <a href="/perfil" class="nome_perfil fit alt">{{ Auth::user()->name }}</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                        <button type="submit" class="button small">LOG OUT</button>
+                    </form>
+                </nav>
+            @endif
+        </header>
 
-    <!-- Footer -->
-    <footer id="footer">
-        <div class="inner">
-            <h2>Entre em Contacto</h2>
-            <ul class="actions">
-                <li><span class="icon fa-phone"></span> <a href="#">(000) 000-0000</a></li>
-                <li><span class="icon fa-envelope"></span> <a href="#">foodtracker@ua.pt</a></li>
-            </ul>
-        </div>
-        <div class="copyright">
-            &copy; Food Tracker. Design <a href="https://templated.co">TEMPLATED</a>. Images <a
-                href="https://unsplash.com">Unsplash</a>.
-        </div>
-    </footer>
+        <!-- Menu -->
+        @if (Auth::check())
+            <nav id="menu">
+                <ul class="links">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/pratos">Pratos</a></li>
+                    <li><a href="/refeicoes">Refeicões</a></li>
+                    <li><a href="/perfil">Perfil</a></li>
+                </ul>
+                <ul class="actions vertical">
+                    <li>
+                        <form action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                            <button type="submit" class="button fit">LOG OUT</button>
+                        </form>
+                    </li> <!-- log out -->
+                </ul>
+            </nav>
+        @endif
 
-    <!-- Scripts -->
-    <script src="/assets/js/jquery.min.js"></script>
-    <script src="/assets/js/jquery.scrolly.min.js"></script>
-    <script src="/assets/js/skel.min.js"></script>
-    <script src="/assets/js/util.js"></script>
-    <script src="/assets/js/main.js"></script>
-	@yield('scripts')
-</body>
+        @yield('content')
+
+        <!-- Footer -->
+        <footer id="footer">
+            <div class="inner">
+                <h2>Entre em Contacto</h2>
+                <ul class="actions">
+                    <li><span class="icon fa-phone"></span> <a href="#">(000) 000-0000</a></li>
+                    <li><span class="icon fa-envelope"></span> <a href="#">foodtracker@ua.pt</a></li>
+                </ul>
+            </div>
+            <div class="copyright">
+                &copy; Food Tracker. Design <a href="https://templated.co">TEMPLATED</a>. Images <a
+                    href="https://unsplash.com">Unsplash</a>.
+            </div>
+        </footer>
+
+        <!-- Scripts -->
+        <script src="/assets/js/jquery.min.js"></script>
+        <script src="/assets/js/jquery.scrolly.min.js"></script>
+        <script src="/assets/js/skel.min.js"></script>
+        <script src="/assets/js/util.js"></script>
+        <script src="/assets/js/main.js"></script>
+        @yield('scripts')
+    </body>
 
 </html>
