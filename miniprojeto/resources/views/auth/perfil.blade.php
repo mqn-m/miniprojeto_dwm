91b2350178d332af71502aa62f3c18f8a5a7ec64
Ozzy <austin.foote@ua.pt>
2020-11-05 17:33:16 +0000

    Moooooore!!!!!

--- miniprojeto/resources/views/auth/perfil.blade.php
+++ miniprojeto/resources/views/auth/perfil.blade.php
@@ -1,57 +1,49 @@
 @extends('layout')
 
 @section('content')
-    <div class="wrapper">
+    <section class="wrapper">
         <div class="inner">
             <h1 class="titulo">{{ __('Alterar Perfil') }}</h1>
-            <div class="content">
+            <div class="row 200%">
+                <div class="12u">
+                    <form method="POST" action="/perfil/{{ Auth::user()->id }}">
+                        @csrf
+                        @method('PUT')
 
-                <form method="POST" action="/perfil/{{ Auth::user()->id }}">
-                    @csrf
-                    @method('PUT')
+                        <div class="row uniform">
+                            <div class="12u$">
+                                <label for="name" class="">{{ __('Nome') }}</label>
+                                <input id="name" type="text" name="name" value="{{ Auth::user()->name }}" required
+                                    autocomplete="name" autofocus>
+                            </div>
 
-                    <div class="row uniform">
-                        <div class="12u$">
-                            <label for="name" class="">{{ __('Nome') }}</label>
-                            <input id="name" type="text" name="name" value="{{ Auth::user()->name }}" required
-                                autocomplete="name" autofocus>
+                            <div class="12u$">
+                                <label for="email" class="">{{ __('E-Mail') }}</label>
+                                <input id="email" type="email" name="email" value="{{ Auth::user()->email }}" required
+                                    autocomplete="email">
+                            </div>
+
+                            <div class="12u$">
+                                <label for="idade" class="">{{ __('Idade') }}</label>
+                                <input id="idade" type="number" name="idade" value="{{ Auth::user()->idade }}" required
+                                    autocomplete="idade">
+                            </div>
+
+                            <div class="12u$">
+                                <label for="peso" class="">{{ __('Peso') }}</label>
+                                <input id="peso" type="number" step="0.010" name="peso" value="{{ Auth::user()->peso }}"
+                                    required autocomplete="peso">
+                            </div>
+
+                            <div class="12u$">
+                                <ul class="actions">
+                                    <li><input type="submit" value="{{ __('Guardar Alterações') }}"></li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="row uniform">
-                        <div class="12u$">
-                            <label for="email" class="">{{ __('E-Mail') }}</label>
-                            <input id="email" type="email" name="email" value="{{ Auth::user()->email }}" required
-                                autocomplete="email">
-                        </div>
-                    </div>
-
-                    <div class="row uniform">
-                        <div class="12u$">
-                            <label for="idade" class="">{{ __('Idade') }}</label>
-                            <input id="idade" type="number" name="idade" value="{{ Auth::user()->idade }}" required
-                                autocomplete="idade">
-                        </div>
-                    </div>
-
-                    <div class="row uniform">
-                        <div class="12u$">
-                            <label for="peso" class="">{{ __('Peso') }}</label>
-                            <input id="peso" type="number" step="0.010" name="peso" value="{{ Auth::user()->peso }}"
-                                required autocomplete="peso">
-                        </div>
-                    </div>
-                    <br>
-
-                    <div class="12u$">
-                        <ul class="actions">
-                            <li><input type="submit" value="{{ __('Guardar Alterações') }}"></li>
-                        </ul>
-                    </div>
-
-                </form>
+                    </form>
+                </div>
             </div>
-
         </div>
-    </div>
+    </section>
 @endsection
--- miniprojeto/resources/views/pratos/novo.blade.php
+++ miniprojeto/resources/views/pratos/novo.blade.php
@@ -1,46 +1,49 @@
 @extends('layout')
 
 @section('content')
-<section class="wrapper">
-    <div class="inner">
-        <h1 class="titulo">Adicionar Prato</h1>
-        <div class="row 200%">
-            <div class="12u">
-                <form method="POST" action="/pratos">
-                    @csrf
-                    <div class="row uniform">
-                        <div class="6u 12u$(xsmall) @error('nome') input_erro @enderror">
-                            <label for="nome">Nome:</label>
-                        <input class="" type="text" name="nome" id="nome" value="{{old('nome')}}" placeholder="Nome"/>
-                            @error('nome')
-                                <p class="erro">{{$errors->first('nome')}}</p>
-                            @enderror
-                        </div>
+    <section class="wrapper">
+        <div class="inner">
+            <h1 class="titulo">Adicionar Prato</h1>
+            <div class="row 200%">
+                <div class="12u">
+                    <form method="POST" action="/pratos">
+                        @csrf
+                        <div class="row uniform">
+                            <div class="6u 12u$(xsmall) @error('nome') input_erro @enderror">
+                                <label for="nome">Nome:</label>
+                                <input class="" type="text" name="nome" id="nome" value="{{ old('nome') }}"
+                                    placeholder="Nome" />
+                                @error('nome')
+                                    <p class="erro">{{ $errors->first('nome') }}</p>
+                                @enderror
+                            </div>
 
-                        <div class="6u$ 12u$(xsmall) @error('cal') input_erro @enderror">
-                            <label for="cal">Calorias:</label>
-                            <input type="number" name="cal" id="cal" value="{{old('nome')}}" placeholder="Calorias" min="0"/>
-                            @error('cal')
-                                <p class="erro">{{$errors->first('cal')}}</p>
-                            @enderror
-                        </div>
+                            <div class="6u$ 12u$(xsmall) @error('cal') input_erro @enderror">
+                                <label for="cal">Calorias:</label>
+                                <input type="number" name="cal" id="cal" value="{{ old('nome') }}" placeholder="Calorias"
+                                    min="0" />
+                                @error('cal')
+                                    <p class="erro">{{ $errors->first('cal') }}</p>
+                                @enderror
+                            </div>
 
-                        <div class="12u$">
-                            <label for="nota">Descrição:</label>
-                            <textarea name="nota" id="nota" placeholder="Descrição" value="{{old('nome')}}" rows="6"></textarea>
+                            <div class="12u$">
+                                <label for="nota">Descrição:</label>
+                                <textarea name="nota" id="nota" placeholder="Descrição" value="{{ old('nome') }}"
+                                    rows="6"></textarea>
+                            </div>
+
+                            <div class="12u$">
+                                <ul class="actions">
+                                    <li><input type="submit" value="Guardar" /></li>
+                                    <li><a href="/pratos"><input type="button" value="Cancelar" class="alt" /></a></li>
+                                </ul>
+                            </div>
                         </div>
-                        
-                        <div class="12u$">
-                            <ul class="actions">
-                                <li><input type="submit" value="Guardar" /></li>
-                                <li><a href="/pratos"><input type="button" value="Cancelar" class="alt" /></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
 
-</section>
+    </section>
 @endsection
