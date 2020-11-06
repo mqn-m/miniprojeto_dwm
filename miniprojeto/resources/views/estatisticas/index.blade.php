556bc10142d8da323a1c6bf7db1af8879814464f
Ozzy <austin.foote@ua.pt>
2020-11-06 20:04:33 +0000

    esta tis ti casssss

--- miniprojeto/resources/views/estatisticas/index.blade.php
+++ miniprojeto/resources/views/estatisticas/index.blade.php
@@ -4,19 +4,14 @@
     <section id="Pratos" class="wrapper">
         <div class="inner" style="text-align: right;">
             <h1 class="titulo">Estatisticas </h1>
-            <div class="12u$">
-                <ul class="actions">
-                    <li><input type="button" class="small special" value="Adicionar" onclick="location.href='/pratos/novo'"></li>
-                </ul>
-            </div>
         </div>
         <div class="inner">
             @if (count($estatisticas) === 0)
                 <h3 style="color: #727a82;" class="sem-pratos-refeicoes">Não tem refeicoes criados ainda</h3>
             @else
-                <div class="gridMasterLOL">
+                <div class="gridMasterLOL gridMasterLOL2">
                     @foreach ($estatisticas as $estatistica)
-                        <div class="cartao">
+                        <div class="cartao cartao2">
                             <div class="content">
                                 <h3 style="word-break: break-all">{{ $estatistica->data_refeicao }}</h3>
                                 <h4>{{ $estatistica->calorias }} cals</h4>
