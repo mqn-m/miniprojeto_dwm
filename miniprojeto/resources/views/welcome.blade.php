c756cd0a11efbace9bf3445e0adf8a3101acb797
Ozzy <austin.foote@ua.pt>
2020-11-06 11:58:26 +0000

    mariaaaaaa albertina

--- miniprojeto/resources/views/welcome.blade.php
+++ miniprojeto/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <section id="banner">
         <div class="content">
             <h1>És o que Comes</h1>
-            <p>Comece a fazer tracking da sua alimentação<br />para depois começar a melhorar os seus comportamentos
+            <p>Comece a fazer tracking da sua alimentaçãoM MARIIAAAAAbr />para depois começar a melhorar os seus comportamentos
                 alimentares</p>
             <ul class="actions">
                 <li><a href="/pratos" class="button scrolly">Pratos</a></li>
